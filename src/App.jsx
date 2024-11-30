36a67e64f307fc60df80b679f85bf9e3f8597a8a
Rifannvl <riffanafendi@gmail.com>
2024-11-30 20:28:33 +0700

    menambahkan path untuk login dan allproduct

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Allproduct from "./components/Allproduct";
+import Login from "./components/auth/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Allproduct />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
