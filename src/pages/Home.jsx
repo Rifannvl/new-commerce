96852b69f35cc41161da5bbe712f1e5937154d39
Rifannvl <riffanafendi@gmail.com>
2024-11-30 20:28:15 +0700

    menghapus main layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,7 @@ import MainLayout from "../layout/MainLayout";
 export default function Home() {
   return (
     <div>
-      <MainLayout>
-        <h1>Home</h1>
-      </MainLayout>
+      <h1>Home</h1>
     </div>
   );
 }
