812d241a5c4e522f86b7fae962d47cd663f399c5
Rifannvl <riffanafendi@gmail.com>
2024-12-01 23:14:48 +0700

    menambahkan footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
 import MainLayout from "../layout/MainLayout";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1>Home</h1>
+      <Footer />
     </div>
   );
 }
