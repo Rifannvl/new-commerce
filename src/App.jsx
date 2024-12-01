31c39450670962bc8ebadfd28cc51c89cfead291
Rifannvl <riffanafendi@gmail.com>
2024-12-02 01:07:13 +0700

    menambahkan route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Ganti Switch dengan Routes
 import Allproduct from "./components/Allproduct";
+import { CartProvider } from "../src/components/context/CardContext"; // Import CartProvider
+import DetailProducts from "./components/DetailProducts";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkut";
 import Login from "./components/auth/Login";
+import Home from "./pages/Home";
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Allproduct />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+    <CartProvider>
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/product" element={<Allproduct />} />
+          {/* Menggunakan element sebagai pengganti component */}
+          <Route path="/product/:id" element={<DetailProducts />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/" element={<Login />} />
+        </Routes>
+      </Router>
+    </CartProvider>
   );
 }
+
+export default App;
