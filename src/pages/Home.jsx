7ff16f5472ccfac754dfbe23e75ad862253883b3
Rifannvl <riffanafendi@gmail.com>
2024-12-02 22:58:50 +0700

    menampilkan bento hero dan slider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
+import BentoHero from "../components/BentoHero";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import SliderProducts from "../components/SliderProducts";
 import MainLayout from "../layout/MainLayout";
 
 export default function Home() {
   return (
     <div className="bg-neutral-900">
       <Header />
-      <h1>Home</h1>
+      <BentoHero />
+      <SliderProducts />
       <Footer />
     </div>
   );
